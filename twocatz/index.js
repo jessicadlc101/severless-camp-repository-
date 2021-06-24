5c5af5e3a6c7865f072ac7a6d92e5ae701184fcf
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-06-24 16:36:00 -0700

    finished twocatz

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,21 +3,46 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let resp = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject", {
-        method: 'GET'
-    });
+    async function getCat() {
+        let resp = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject", {
+            method: 'GET'
+        });
+        
+        let data = await resp.arrayBuffer()
+        // we need to receive it as a buffer since this is an image we are receiving from the API
+        // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+        
+        var base64data = Buffer.from(data).toString('base64')
+    //put what you want to turn into base64 inside "originaldata"
+    //"originaldata" will be encoded in base64.  
+    return base64data
+    } 
+    let catpic1 = await getCat() 
+    let catpic2 = await getCat() 
+
+    async function getNames () {
+        var names =["Shreya", "Emily", "Fifi", "Evelyn", "Julia", "Daniel", "Fardeen"]
+        var random_value = Math.floor(names.length * Math.random())
+        var resultname = names[random_value] 
+            
+        
+        return resultname 
+    }
     
-    let data = await resp.arrayBuffer()
-    // we need to receive it as a buffer since this is an image we are receiving from the API
-    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    
-    var base64data = Buffer.from(data).toString('base64')
-//put what you want to turn into base64 inside "originaldata"
-//"originaldata" will be encoded in base64.
+    let name1 = getNames() 
+    let name2 = getNames()
 
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {base64data}
+        body: {
+            catpic1: catpic1,
+            catpic2: catpic2, 
+            names: [name1, name2] 
+        }     
     };
+}  
+
+var JSON = { 
+        "key" : "value"
 }
