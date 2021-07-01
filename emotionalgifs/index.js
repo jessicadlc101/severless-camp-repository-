efa2fdac97b031669acfc64e0c0df3520dd9b3e6
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-01 13:27:37 -0700

    emotionalgifs

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,44 +1,20 @@
 var multipart = require('parse-multipart');
-var fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var boundary = multipart.getBoundary(req.headers['content-type']);
-    var body = req.body 
-    var parts = mutlipart.Parse(body, boundary); 
-    var image = parts[0].data 
+  
+    var body = req.body
 
-    var result = await analyzeImage(imageData)
+    var parts = multipart.Parse(body, boundary); 
+
+    var imageData = parts[0].data 
+
+    var convertedResult = Buffer.from(imageData).toString('base64')
+    
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: result 
-    }; 
-    console.log(result) 
-    context.done();
-} 
-
-async function analyzeImage(img){
-    
-    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect'; 
-
-    let params = new URLSearchParams({
-        'returnFaceId': 'true',
-        'returnFaceAttributes': 'emotion'    
-    }) 
-
-    let resp = await fetch(uriBase + '?' + params.toString(), {
-        method: 'POST',  
-        body: img, 
-        
-        headers: {
-            'Content-type': 'application/octet-stream',
-            'Ocp-Apim-Subscription-Key': SUBSCRIPTIONKEY
-        }
-
-    })
-    let emotionData = await resp.json(); 
-    
-    return emotionData;  
+        body: convertedResult 
+    };
 }
