cd6d30269a9b25206f30b6bcabeba65dc01a6dfc
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-01 17:56:35 -0700

    main emotion detected

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -11,8 +11,14 @@ module.exports = async function (context, req) {
 
     var result = await analyzeImage(image);
     
+    let emotions = result[0].faceAttributes.emotion;
+
+    let objects = Object.values(emotions);
+
+    const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+  
     context.res = {
-	    body: {result}
+	    body: main_emotion
     }; 
 }
 
