428c381339d7675e738b10b9acc196d748a5784d
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-01 17:34:59 -0700

    added face api

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -5,41 +5,37 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var boundary = multipart.getBoundary(req.headers['content-type']);
-
     var body = req.body
+    var parts = multipart.Parse(body, boundary); 
+    var image= parts[0].data
 
-    var parts = multipart.Parse(body, boundary);
-
-    var result = await analyzeImage(imageData);
+    var result = await analyzeImage(image);
+    
     context.res = {
-	body: {
-		    result
-	  }
-    
-    };
-    console.log(result)
-    context.done(); 
-    
+	    body: {result}
+    }; 
 }
 
 async function analyzeImage(img){
-    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect'; 
+    const subscriptionKey = process.env.FACEAPI_KEY1;
+    const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect'; 
     
     let params = new URLSearchParams({
         'returnFaceId': 'true',
         'returnFaceAttributes': 'emotion'     
     
     })
+    
     let resp = await fetch(uriBase + '?' + params.toString(), {
         method: 'POST',  
         body: img,  
       
         headers: {
-            'Content-Type': 'application-octet-stream',
+            'Content-Type': 'application/octet-stream',
             'Ocp-Apim-Subscription-Key': subscriptionKey
         }
     }) 
-let emotionData = await resp.json() 
-return emotionData
+    let emotionData = await resp.json(); 
+
+    return emotionData;
 }
