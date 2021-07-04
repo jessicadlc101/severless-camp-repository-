a822d6f405e3c28cc0d88e47689783a0c674bf87
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-03 22:36:31 -0700

    spotify generation exercise completed

--- song4u/index.js
+++ song4u/index.js
@@ -15,11 +15,22 @@ module.exports = async function (context, req) {
     let age_data = await analyzeImage(data) 
     let age = age_data[0].faceAttributes.age
     
-    let generation = determine_generation(age)
+    let generation = determine_generation(age) 
+
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+        "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+        "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+        "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+        "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    let song = songs[generation] 
     
+    let response = `We guessed you're part of this generation: ${generation}! Happy listening! ${song}`
+    
+    context.log(response)
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: generation
+        body: response
     };
 }   
 
