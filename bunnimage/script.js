125404cb678e12d51214cc4a5b3916f0a8d264a7
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-15 14:28:48 -0700

    completed step 3

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,8 +1,36 @@
 function getImage(event) { 
     event.preventDefault()
-    if (document.getElementById('name').value != '') {
-        $('#output').text("Thanks!")
+    var myform = document.getElementById("myform")
+    let nameInput = document.getElementById("username"); 
+    let fileInput = document.getElementById("image");
+    let file = fileInput.files[0]; 
+    
+    var payload = new FormData(myform);
+    console.log(payload)
+    payload.append("file", file); 
+    $('#output').text("Thanks!")
+    
+    if (document.getElementById('username').value != '') {
+        
+        try { 
+            let url = "https://morse1.azurewebsites.net/api/bunnimage-upload?"
+            console.log("Image was uploaded, making POST request to Azure function")
+            
+            const resp = await fetch(url, { 
+                method: 'POST', 
+                headers: {  
+                   'codename': nameInput.value
+               }, 
+               body: payload 
+           }) 
+           $('#output').text("Your image has been stored successfully!")
+        }catch(err) { 
+            $('#output').text(err)
+        }
+        
     }else { 
         alert("No name error.")
-    }
+    } 
+
+    
 }
