2007f02a309dfee2c74661338d7a6fe546c56ae6
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-17 22:28:02 -0700

    completed step 4

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,37 +1,70 @@
-function getImage(event) { 
-    event.preventDefault()
-    var myform = document.getElementById("myform")
-    let nameInput = document.getElementById("name");
-    let fileInput = document.getElementById("image");
+async function getImage(event) { 
+    event.preventDefault();
+    var myform = document.getElementById('myform')
+    let nameInput = document.getElementById('username');
+    let fileInput = document.getElementById('image');
     let file = fileInput.files[0]; 
     
     var payload = new FormData(myform);
-    console.log(payload)
+    console.log(payload);
     payload.append("file", file);
-    $('#output').text("Thanks!")
+    $('#output').text("Thanks!");
     
-    if (document.getElementById('name').value != '') {
+    if (document.getElementById('username').value != '') {
         
         try { 
-            let url = "https://morse1.azurewebsites.net/api/bunnimage-upload?"
-            console.log("Image was uploaded, making POST request to Azure function")
+            let url = "https://morse1.azurewebsites.net/api/bunnimage-upload?";
+            console.log("Image was uploaded, making POST request to Azure function");
             
-            const resp = fetch(url, { 
+            const resp = await fetch(url, { 
                 method: 'POST', 
                 headers: {  
-                   'codename': nameInput.value
+                   "codename": nameInput.value
                }, 
                body: payload 
-           }) 
+           }); 
            console.log("POST request was made successfully")
-           $('#output').text("Your image has been stored successfully!")
+           $('#output').text('Your image has been stored successfully!');
         }catch(err) { 
-            $('#output').text(err)
+            $('#output').text(err);
         }
         
     }else { 
-        alert("No name error.")
-    } 
+        alert("No name error.");
+    }  
 
-    
 }
+
+async function downloadImage() { 
+  let username= document.getElementById('downloadusername').value  
+  
+    
+  if(username != '') {  
+    
+    try {
+      let url = "https://morse1.azurewebsites.net/api/bunnimage-download?" 
+      
+      console.log("Got file name, making GET request to download image");
+
+      const resp = await fetch(url, { 
+        method: 'GET',
+        headers: { 
+          "username": username
+        } 
+      })
+      let data = await resp.json ()
+            let imageUrl = data.downloadUri
+        
+            console.log("Made GET request successfully");
+        
+            window.open(imageUrl, "_self")
+        
+         } catch(err) {
+            alert(err)
+         }
+
+        
+        } else {
+          alert('No name error.');
+        }
+      }       
