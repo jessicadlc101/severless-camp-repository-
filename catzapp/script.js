7a0d249b8c668bbffe04c61ea400350d6311a2d0
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-18 22:11:20 -0700

    completed step 5

--- catzapp/script.js
+++ catzapp/script.js
@@ -1,7 +1,7 @@
 function y1k3s() { 
     let name = document.getElementById("name").value 
     
-    let endpoint = "https://cataas.com/cat/cute/says/" + name
+    let endpoint = "https://cataas.com/cat/says/cat" + name
     if(name != '') { 
         document.getElementById("image").src = endpoint
     
