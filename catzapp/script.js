f0dd2b2d9571d4912cfcc33544a3e05ab13f7ecf
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-23 14:54:51 -0700

    completed course

--- catzapp/script.js
+++ catzapp/script.js
@@ -1,26 +1,26 @@
 function y1k3s() { 
     let name1 = document.getElementById("name1").value 
     
-    let endpoint1 = "https://cataas.com/cat/says/cat" + name1
+    let endpoint1 = "https://cataas.com/cat/cute/says/" + name1
     if(name1 != '') { 
         document.getElementById("image1").src = endpoint1
     
     } 
     let name2 = document.getElementById("name2").value 
     
-    let endpoint2 = "https://cataas.com/cat/says/cat" + name2
+    let endpoint2 = "https://cataas.com/cat/cute/says/" + name2
     if(name2 != '') { 
         document.getElementById("image2").src = endpoint2 
     } 
     let name3 = document.getElementById("name3").value 
     
-    let endpoint3 = "https://cataas.com/cat/says/cat" + name3
+    let endpoint3 = "https://cataas.com/cat/cute/says/" + name3
     if(name3 != '') { 
         document.getElementById("image3").src = endpoint3 
     } 
     let name4 = document.getElementById("name4").value 
     
-    let endpoint4 = "https://cataas.com/cat/says/cat" + name4
+    let endpoint4 = "https://cataas.com/cat/cute/says/" + name4
     if(name4 != '') { 
         document.getElementById("image4").src = endpoint4 
     }
