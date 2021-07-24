f529bb3e7b1975ec3abadf79c1fdcf822a40dfd4
Jess DeLaCruz <jesdelac@ucsc.edu>
2021-07-23 19:46:57 -0700

    completed step 7

--- catzapp/script.js
+++ catzapp/script.js
@@ -3,25 +3,45 @@ function y1k3s() {
     
     let endpoint1 = "https://cataas.com/cat/cute/says/" + name1
     if(name1 != '') { 
-        document.getElementById("image1").src = endpoint1
+        document.getElementById("image1").src = base64Prefix + endpoint1;
     
     } 
     let name2 = document.getElementById("name2").value 
     
     let endpoint2 = "https://cataas.com/cat/cute/says/" + name2
     if(name2 != '') { 
-        document.getElementById("image2").src = endpoint2 
+        document.getElementById("image2").src = base64Prefix + endpoint2; 
     } 
     let name3 = document.getElementById("name3").value 
     
     let endpoint3 = "https://cataas.com/cat/cute/says/" + name3
     if(name3 != '') { 
-        document.getElementById("image3").src = endpoint3 
+        document.getElementById("image3").src = base64Prefix + endpoint3; 
     } 
     let name4 = document.getElementById("name4").value 
     
     let endpoint4 = "https://cataas.com/cat/cute/says/" + name4
     if(name4 != '') { 
-        document.getElementById("image4").src = endpoint4 
+        document.getElementById("image4").src = base64Prefix + endpoint4; 
     }
+} 
+
+async function getCatPhoto(name) {
+    try{
+        let url = "https://morse1.azurewebsites.net/api/twocatz?";
+    fetch(url, {
+    headers: {
+    "name": name
+    }
+ })
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+    });
 }
+     catch(err){
+      alert(err);
+     }
+ } 
